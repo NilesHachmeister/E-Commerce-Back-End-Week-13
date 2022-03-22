a66374f3b93517d140defcf212af82f3e0a2e8dd
NilesHachmeister <93550726+NilesHachmeister@users.noreply.github.com>
2022-03-22 13:15:48 -0600

    added the get functionality for categories

--- models/index.js
+++ models/index.js
@@ -8,12 +8,13 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
+  onDelete: 'CASCADE',
 })
 
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id',
-  onDelete: 'SET NULL|CASCADE',
+  onDelete: 'CASCADE',
 });
 
 // Products belongToMany Tags (through ProductTag)
